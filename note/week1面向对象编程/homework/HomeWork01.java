9c120ca057cc2e37a8115182e5ee91d7d8371c94
lppppp <lipeng1614260149@gmail.com>
2025-03-11 22:39:37 +0800

    训练题解答

---
+++ note/week1面向对象编程/homework/HomeWork01.java
@@ -0,0 +1,21 @@
+package homework;
+/*编写类A01，定义方法max，实现求某个方法的最大值，并返回*/
+public class HomeWork01 {
+    public static void main(String[] args){
+        A01 a1 = new A01();
+        double[] arr = {77,99,187};
+        System.out.println(a1.max(arr));
+    }
+}
+
+class A01{
+    public double max(double[] a){
+        double m = 0;
+        for(int i=0;i<a.length;i++){
+            if(m<a[i]){
+                m = a[i];
+            }
+        }
+        return m;
+    }
+}
