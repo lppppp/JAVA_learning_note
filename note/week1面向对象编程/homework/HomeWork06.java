57065c57fd6a0aa614e09028642adbbcc21cbdbc
lppppp <lipeng1614260149@gmail.com>
2025-03-15 21:54:41 +0800

    新增作业代码

---
+++ note/week1面向对象编程/homework/HomeWork06.java
@@ -0,0 +1,30 @@
+package homework;
+/*练习：创建一个Drawable接口，包含一个draw()方法。
+然后创建Circle和Square类实现Drawable接口，并在main方法中创建这些对象并调用draw()方法。*/
+public class HomeWork06 {
+    public static void main(String[] args){
+        Drawable d1 = new Circle();
+        d1.draw();
+
+        Drawable d2 = new Square();
+        d2.draw();
+    }
+}
+//定义接口
+interface Drawable{
+    void draw();//方法签名
+}
+
+class Circle implements Drawable{
+    @Override
+    public void draw(){
+        System.out.println("Draw circle");
+    }
+}
+
+class Square implements Drawable{
+    @Override
+    public void draw(){
+        System.out.println("Draw square");
+    }
+}
